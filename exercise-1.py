b003521f3f1f3eb6d5e68bef0dd210f42aae4c89
KheavKienghok <156294487+KheavKienghok@users.noreply.github.com>
2024-03-14 21:19:29 +0700

    Add files via upload

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,12 @@
 def replace_last(numbers):
-    ...
+    
+    if not numbers:
+        return []
+    
+    num = len(numbers)
+    result = numbers[num - 1]
+
+    numbers.pop()
+    numbers.append(result)
+    
+    return numbers
