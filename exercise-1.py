51347b26739a473baa84a5b1ad1a09b390f7b7b7
w3schools-test <2023500Kheav@aupp.edu.kh>
2024-03-15 10:09:25 +0700

    excercise

--- exercise-1.py
+++ exercise-1.py
@@ -1,12 +1,5 @@
 def replace_last(numbers):
     
-    if not numbers:
-        return []
+    result = numbers[-1:] + numbers[:-1]
     
-    num = len(numbers)
-    result = numbers[num - 1]
-
-    numbers.pop()
-    numbers.append(result)
-    
-    return numbers
+    return result
